242f0d8329b0fbf0c423e0f81c1179f5c2f6123d
Matheus Lourenço <136980634+silvamls@users.noreply.github.com>
2026-09-09 14:44:23 -0100

    Update Catch_models.R

--- Catch models/Catch_models.R
+++ Catch models/Catch_models.R
@@ -1,1 +1,13 @@
-Catch models
+#-------------------------------------------------------------------------------------------#
+#     Este script contem toda analise baseada em captura do Decapterus macarellus           #
+#   Analises possuem foco para o Decapterus macarellus no arquipelago de Cabo verde         #
+#   Hipóteses de estoques consideradas h1: estoque unitário para o arquipélago              #
+#       h2: estoques unitarios para cada ilha ou ilhas barlavento e sotavento               #
+#          Modelos baseados em captura: DB-SRA (Dick & MacCall, 2011)                       #
+#     Modelo utilizado em seguida: CMSY (Froese et al., 2023)                               #
+# Analises de sensibilidade e diferentes hipoteses assumidas de historia de vida, taxa      # 
+#   intrinseca de crescimento (r) e deplecoes de biomassa (B/k) para o fim da serie         #
+# Codificacao criada por Silva, MLS ; 09/09/2026, Instituto do Mar- IMar, Mindelo           #
+#-------------------------------------------------------------------------------------------#
+
+
