e5b6d269931c5091b59ccf2ef2bb2ec6cd9f30fc
Matheus Lourenço <136980634+silvamls@users.noreply.github.com>
2026-09-14 14:22:54 -0100

    att

--- Catch and index models/Catch and index models.R
+++ Catch and index models/Catch and index models.R
@@ -1,14 +1,15 @@
-#-------------------------------------------------------------------------------------------#
-#     Este script contem toda analise baseada em captura do Decapterus macarellus           #
-#   Analises possuem foco para o Decapterus macarellus no arquipelago de Cabo verde         #
-#   Hipóteses de estoques consideradas h1: estoque unitário para o arquipélago              #
-#       h2: estoques unitarios para cada ilha ou ilhas barlavento e sotavento               #
-#          Modelos baseados em captura: DB-SRA (Dick & MacCall, 2011)                       #
-#     Modelo utilizado em seguida: CMSY (Froese et al., 2023)                               #
-# Analises de sensibilidade e diferentes hipoteses assumidas de historia de vida, taxa      # 
-#   intrinseca de crescimento (r) e deplecoes de biomassa (B/k) para o fim da serie         #
-# Codificacao criada por Silva, MLS ; 09/09/2026, Instituto do Mar- IMar, Mindelo           #
-#-------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------------#
+#     Este script contem toda analise baseada em captura e abundância do Decapterus macarellus           #
+#   Analises possuem foco para o Decapterus macarellus no arquipelago de Cabo verde                      #
+#              utilizando o JABBA- Just Another Bayesian Biomass Assessment (Winker et al., 2018)        #
+#                Bayesian state-space surplus production model — BSSPM                                   #
+#       Hipóteses de estoques consideradas h1: estoque unitário para o arquipélago                       #
+#         h2: estoques unitarios para cada ilha ou ilhas barlavento e sotavento                          #
+#   tentativa de padronização de CPUE, inclusão de mudança de direcionamento/mudança de alvo no tempo    #
+#  Desmembramento de CPUE entre as espécies do D. macarellus e Auxis spp.                                #  
+# utilização de modelos regressivos (Generalized Linear models - GLM) e Generalized additive models (GAM)#
+#            # Codificacao criada por Silva, MLS ; 09/09/2026, Instituto do Mar- IMar, Mindelo           #
+#--------------------------------------------------------------------------------------------------------#
 
 # limpando ambiente de trabalho...
 rm(list = ls())
